b70551a6bfab42ba13de222b976358e52f561b0f
Your Name <you@example.com>
2026-07-12 20:26:42 +0900

    Fix PWA icon path

--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -1,24 +1,13 @@
-const CACHE_NAME = "calendar-v1";
-
-const urlsToCache = [
-    "/",
-    "/manifest.json"
-];
-
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
-    );
+self.addEventListener("install", function(event) {
+    console.log("Service Worker installed");
 });
 
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-    );
+
+self.addEventListener("activate", function(event) {
+    console.log("Service Worker activated");
+});
+
+
+self.addEventListener("fetch", function(event) {
+
 });
