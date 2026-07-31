d2424182a46d823a181d3d895c8d653d46476382
Your Name <you@example.com>
2026-07-31 09:42:57 +0900

    スマホメニュー改善

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -863,3 +863,32 @@ if ("serviceWorker" in navigator) {
     });
 
 }
+// ==========================
+// スマホ サイドバー開閉
+// ==========================
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    const menuBtn =
+        document.getElementById("menuBtn");
+
+
+    const sidebar =
+        document.querySelector(".sidebar");
+
+
+    if(menuBtn && sidebar){
+
+
+        menuBtn.addEventListener("click", function(){
+
+
+            sidebar.classList.toggle("active");
+
+
+        });
+
+
+    }
+
+});
